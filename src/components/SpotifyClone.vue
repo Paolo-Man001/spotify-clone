f0c12696769cabf3ad1c543f26dcb6a6df469213
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-10-05 22:59:00 +1300

    SpotifyClone - Initial General Layout

--- src/components/SpotifyClone.vue
+++ src/components/SpotifyClone.vue
@@ -1,6 +1,105 @@
 <template>
-  <div>
-    <h1>Spotify Clone</h1>
+  <div class="flex flex-col h-screen">
+    <div class="bg-blue-500 flex-1 flex overflow-y-hidden">
+      <!-- Sidebar -->
+      <div class="sidebar bg-red-500 w-48 flex-none flex flex-col justify-between">
+        <div>one</div>
+        <div class="overflow-y-auto">
+          <h1>Middle</h1>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi nemo eaque similique reprehenderit, rem labore iure corporis, perferendis beatae praesentium, unde debitis numquam laboriosam ullam perspiciatis pariatur facere dolores tenetur cupiditate nihil at ipsam. Fugiat quos molestias error nulla nesciunt delectus amet minima labore! Omnis asperiores voluptatum aliquid beatae maiores perspiciatis aut itaque, officia, ex, sed reiciendis velit labore ipsum a quam at numquam esse eligendi eum! Facere voluptatibus suscipit expedita iusto dolores recusandae ab beatae commodi numquam? Molestiae aspernatur maiores impedit consectetur natus voluptatum voluptas at expedita laudantium.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi nemo eaque similique reprehenderit, rem labore iure corporis, perferendis beatae praesentium, unde debitis numquam laboriosam ullam perspiciatis pariatur facere dolores tenetur cupiditate nihil at ipsam. Fugiat quos molestias error nulla nesciunt delectus amet minima labore! Omnis asperiores voluptatum aliquid beatae maiores perspiciatis aut itaque, officia, ex, sed reiciendis velit labore ipsum a quam at numquam esse eligendi eum! Facere voluptatibus suscipit expedita iusto dolores recusandae ab beatae commodi numquam? Molestiae aspernatur maiores impedit consectetur natus voluptatum voluptas at expedita laudantium.
+        </div>
+        <div>three</div>
+        
+      </div>
+      <!-- Content-Area -->
+      <div class="content-area bg-red-300 flex-1 overflow-y-auto">
+        <h1>Content Area</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci explicabo voluptatem eum
+        dolor enim expedita ab porro quae. Esse aliquid non incidunt eaque asperiores ipsa quasi,
+        distinctio iste numquam ipsam dicta soluta aspernatur architecto, ducimus quibusdam nobis.
+        Voluptates sequi architecto in rerum! Quisquam delectus magni, nostrum consectetur hic nisi
+        doloribus repellat doloremque qui recusandae. Esse hic distinctio sint voluptates aut
+        voluptate expedita consectetur adipisci ipsum? Optio quaerat similique illo iure sequi ea
+        velit ullam non maxime harum officiis ad, sunt aperiam quibusdam iste est distinctio
+        repudiandae magni dolorum delectus illum repellat obcaecati labore. Eum id illo minima
+        inventore soluta! Esse molestiae expedita accusantium, ipsa obcaecati veniam maiores
+        molestias odio minima magnam? Quod explicabo animi id ipsa, cum nisi delectus ipsam nam
+        aperiam molestias sed. Optio quasi cum expedita officia dolor fugiat perferendis ipsum, fuga
+        illo tempore quod suscipit doloribus distinctio. Nostrum iure cupiditate fugit totam,
+        commodi placeat optio architecto! Nisi provident voluptatem beatae alias saepe nihil
+        voluptate eligendi, libero at a veritatis voluptates vel dolorum sit reprehenderit tenetur
+        quos totam doloremque. Maiores magnam libero fuga? Harum ipsa nam eius consectetur, officiis
+        maxime nihil quae aspernatur eaque officia atque iure id obcaecati vitae est ipsum ea illo!
+        Tempore, voluptatum modi? Commodi, ullam quae nostrum temporibus saepe qui dicta quo
+        asperiores ea perferendis alias quis assumenda adipisci, soluta laboriosam. Ea quibusdam,
+        incidunt rerum perspiciatis earum rem ab maxime laudantium laborum ducimus architecto
+        maiores ut repellendus iusto mollitia ullam nobis voluptate dolor eaque deserunt iure.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci explicabo voluptatem eum
+        dolor enim expedita ab porro quae. Esse aliquid non incidunt eaque asperiores ipsa quasi,
+        distinctio iste numquam ipsam dicta soluta aspernatur architecto, ducimus quibusdam nobis.
+        Voluptates sequi architecto in rerum! Quisquam delectus magni, nostrum consectetur hic nisi
+        doloribus repellat doloremque qui recusandae. Esse hic distinctio sint voluptates aut
+        voluptate expedita consectetur adipisci ipsum? Optio quaerat similique illo iure sequi ea
+        velit ullam non maxime harum officiis ad, sunt aperiam quibusdam iste est distinctio
+        repudiandae magni dolorum delectus illum repellat obcaecati labore. Eum id illo minima
+        inventore soluta! Esse molestiae expedita accusantium, ipsa obcaecati veniam maiores
+        molestias odio minima magnam? Quod explicabo animi id ipsa, cum nisi delectus ipsam nam
+        aperiam molestias sed. Optio quasi cum expedita officia dolor fugiat perferendis ipsum, fuga
+        illo tempore quod suscipit doloribus distinctio. Nostrum iure cupiditate fugit totam,
+        commodi placeat optio architecto! Nisi provident voluptatem beatae alias saepe nihil
+        voluptate eligendi, libero at a veritatis voluptates vel dolorum sit reprehenderit tenetur
+        quos totam doloremque. Maiores magnam libero fuga? Harum ipsa nam eius consectetur, officiis
+        maxime nihil quae aspernatur eaque officia atque iure id obcaecati vitae est ipsum ea illo!
+        Tempore, voluptatum modi? Commodi, ullam quae nostrum temporibus saepe qui dicta quo
+        asperiores ea perferendis alias quis assumenda adipisci, soluta laboriosam. Ea quibusdam,
+        incidunt rerum perspiciatis earum rem ab maxime laudantium laborum ducimus architecto
+        maiores ut repellendus iusto mollitia ullam nobis voluptate dolor eaque deserunt iure.
+        Inventore explicabo iure dolorum ullam iste eius aut, aperiam animi facere, culpa quasi
+        aspernatur porro temporibus saepe totam vitae a, cumque amet harum dicta accusantium! Dolor
+        eum odit aspernatur dolorum nemo deserunt neque molestias deleniti fugiat quis. Non
+        cupiditate ex similique fugit! Nesciunt dignissimos corporis explicabo quia sit odio
+        voluptate cumque architecto? Est sequi assumenda voluptas nemo recusandae natus, dolore
+        minima esse numquam atque officia, accusantium quae molestiae adipisci pariatur repellat
+        obcaecati quibusdam? Iure temporibus, deleniti, minus suscipit error aliquam aliquid,
+        tempora alias tempore velit sed tenetur quod neque minima nam cupiditate eveniet dignissimos
+        sunt? Neque odio possimus sequi nisi dolore dolorum, dicta delectus itaque, laboriosam rerum
+        dolores excepturi officiis quasi consectetur praesentium voluptates. Incidunt officia
+        dolorum beatae eveniet sed quis consectetur pariatur ipsum animi at excepturi maiores
+        tempora quos magni quasi iusto quas eum unde, voluptatibus optio alias aperiam id laborum
+        facere? Ea error minus, quasi odit voluptatibus atque possimus commodi deleniti ipsum ut
+        laboriosam, a repudiandae voluptates magni officiis, suscipit quidem veritatis omnis sit
+        laudantium obcaecati accusamus. Voluptas corporis atque quibusdam magni culpa iure suscipit
+        soluta voluptate omnis quae, voluptatum, veniam doloremque nostrum natus esse iste molestiae
+        non ut sed commodi at sint ullam illo. Quis in, distinctio dolorum esse eligendi unde quia
+        libero ut, nesciunt incidunt alias voluptatem architecto totam quo et beatae expedita eaque
+        laudantium adipisci autem! Rem voluptatibus cum aut dolore porro excepturi amet blanditiis
+        delectus iusto aliquid architecto tempore doloribus voluptas cumque, enim nesciunt dolorem
+        tempora quaerat ut reprehenderit, a veritatis? Similique, sit, id facilis assumenda corporis
+        voluptatem eveniet illum vitae veniam aliquid voluptas. Dolorem quam doloremque illo culpa
+        nisi voluptatibus blanditiis itaque impedit quos perspiciatis vel quia corporis sequi sit
+        quidem, sint earum dolore odit illum a deleniti nulla accusantium. Commodi, nobis
+        exercitationem facilis quisquam cupiditate optio explicabo, magni sit minima suscipit,
+        sapiente temporibus quae nam hic corrupti! Itaque asperiores quibusdam praesentium sit?
+        Consequuntur rem fugit ducimus fugiat voluptatem ab voluptas provident quaerat, voluptatibus
+        maiores possimus atque eius? Et repellat laudantium non, molestias voluptatem alias
+        blanditiis ab placeat cumque, inventore sit praesentium eos veniam quae facere? Aperiam
+        tempora vero ratione beatae quos necessitatibus cumque neque cupiditate possimus? Minus a
+        molestias vitae esse veniam ipsa, dolore quo eos, dolores exercitationem nobis id tempora
+        deserunt cupiditate necessitatibus rerum, saepe nesciunt? Atque laudantium, temporibus non
+        iste harum sint, voluptatum molestias aliquid amet eos consequatur saepe numquam natus
+        laboriosam nulla deleniti asperiores? Totam tenetur officiis eos rerum natus. Dolores
+        voluptatibus quasi quod incidunt nesciunt quam blanditiis, voluptates obcaecati minus nisi.
+        Pariatur veritatis quam molestiae optio labore sapiente dolorum distinctio incidunt. Quae
+        itaque tenetur culpa blanditiis fugiat. Atque veritatis ad iste earum excepturi. Error optio
+        saepe quia maxime reprehenderit itaque delectus dolor qui, tempora, eius sapiente ipsum
+        tenetur mollitia, inventore molestiae suscipit perferendis neque expedita sit?
+      </div>
+    </div>
+    <!-- Bottom -->
+    <div class="bg-blue-300">
+      bottom
+    </div>
   </div>
 </template>
 
